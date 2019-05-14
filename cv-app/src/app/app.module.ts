5962771e24dba0ffd57c87e4c176791c427f00fa
Pawel Wojtak <pawel.wojtak@globallogic.com>
2019-05-14 13:06:05 +0200

    add modules

--- cv-app/src/app/app.module.ts
+++ cv-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
